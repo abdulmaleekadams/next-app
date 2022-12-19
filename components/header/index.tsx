4ad44aff45fca4edb48d7631879d768a8ce81e5d
abdulmaleekadams <harbdulmaleek@gmail.com>
2022-12-19 02:52:14 -0800

    structure code

---
+++ components/header/index.tsx
@@ -0,0 +1,11 @@
+import { container, title } from './styles'
+
+const Header = () => {
+  return (
+    <header css={container}>
+      <h1 css={title}>Header</h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/header/styles.ts
@@ -0,0 +1,19 @@
+import { css } from "@emotion/react";
+
+const container = css`
+    min-height: 100vh;
+    padding: 0 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const title = css`
+    text-align: center;
+    font-size: 4rem;
+    line-height: 1.15;
+    margin: 0;
+`
+export { 
+    container, title
+}
