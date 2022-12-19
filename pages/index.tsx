c20e59dd05eefc535f4d1761f726e0a5d0aa9c46
abdulmaleekadams <harbdulmaleek@gmail.com>
2022-12-19 05:29:04 -0800

    add layout to path in ts config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,5 @@
-import Head from 'next/head'
-import Header from '@components/header'
-import Navigation from '@components/navigation'
+import Page from '@layouts/pages'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>All about NextJS</title>
-      </Head>
-      <main>
-        <Header />
-        <Navigation />
-      </main>
-    </>
-  )
+  return <Page />
 }
