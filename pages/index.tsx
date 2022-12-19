818dc9e24a79b24c7e34f426318a7470825d67b5
abdulmaleekadams <harbdulmaleek@gmail.com>
2022-12-19 01:39:32 -0800

    setup emotion/react

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
+          <p></p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
